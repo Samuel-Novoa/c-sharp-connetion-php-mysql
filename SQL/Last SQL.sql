4f8f1001f7253ce9ab99685e476d9e2048d94585
Samuel Novoa <82002858+Samuel-Novoa@users.noreply.github.com>
2023-09-09 12:18:19 -0500

    Create Last SQL.sql

---
+++ SQL/Last SQL.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE productos;
+
+CREATE TABLE productos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    codigo VARCHAR(50) NOT NULL,
+    precio DOUBLE NOT NULL,
+    cantidad_stock INT NOT NULL,
+    fecha_creacion DATETIME NOT NULL
+);
